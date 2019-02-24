114e29c6f18e0b1931d3a12d550db212266d2f6f
JoeDeMabior <39078169+JoeDeMabior@users.noreply.github.com>
2019-02-24 16:02:11 +0300

    Update and delete data with MongoDB

--- mongodb/routes/index.js
+++ mongodb/routes/index.js
@@ -1,55 +1,84 @@
 const express = require('express');
 const router = express.Router();
-const mongo = require('mongodb');
+const mongo = require('mongodb').MongoClient;
+const objectId = require('mongodb').ObjectID;
 const assert = require('assert');
 
-const url = 'mongodb://127.0.0.1:27017/test';
+const url = 'mongodb://localhost:27017';
 
 /* GET home page. */
-router.get('/', function (req, res, next) {
+router.get('/', function(req, res, next) {
     res.render('index');
 });
 
-router.get('/get-data', function (req, res, next) {
+router.get('/get-data', function(req, res, next) {
     const resultArray = [];
-    mongo.connect(url, function (err, db) {
-        assert.strictEqual(null, err);
-        const cursor = db.collection('user-data').find();
-        cursor.forEach(function (doc, err) {
-            assert.strictEqual(null, err);
+    mongo.connect(url, function(err, client) {
+        assert.equal(null, err);
+        const dbName = client.db('test');
+        const cursor = dbName.collection('user-data').find();
+        cursor.forEach(function(doc, err) {
+            assert.equal(null, err);
             resultArray.push(doc);
-        }, function () {
-            db.close();
-            res.render('index', {items: resultArray})
-        })
-    })
+        }, function() {
+            client.close();
+            res.render('index', {items: resultArray});
+        });
+    });
 });
 
-router.post('insert', function (req, res, next) {
+router.post('/insert', function(req, res, next) {
     const item = {
         title: req.body.title,
         content: req.body.content,
         author: req.body.author
     };
 
-    mongo.connect(url, function (err, db) {
-        assert.strictEqual(null, err);
-        db.collection('user-data').insertOne(item, function (err, result) {
-            assert.strictEqual(null, err);
-            console.log('Item inserted.');
-            db.close()
-        })
+    mongo.connect(url, function(err, client) {
+        assert.equal(null, err);
+        const dbName = client.db('test');
+        dbName.collection('user-data').insertOne(item, function(err, result) {
+            assert.equal(null, err);
+            console.log('Item inserted');
+            client.close();
+        });
     });
 
-    res.redirect('/')
+    res.redirect('/');
 });
 
-router.post('update', function (req, res, next) {
+router.post('/update', function(req, res, next) {
+    const item = {
+        title: req.body.title,
+        content: req.body.content,
+        author: req.body.author
+    };
 
+    const id = req.body.id;
+
+    mongo.connect(url, function(err, client) {
+        assert.equal(null, err);
+        const dbName = client.db('test');
+        dbName.collection('user-data').updateOne({"_id": objectId(id)}, {$set: item}, function(err, result) {
+            assert.equal(null, err);
+            console.log('Item updated');
+            client.close();
+        });
+    });
 });
 
-router.post('delete', function (req, res, next) {
+router.post('/delete', function(req, res, next) {
+    const id = req.body.id;
 
+    mongo.connect(url, function(err, client) {
+        assert.equal(null, err);
+        const dbName = client.db('test');
+        dbName.collection('user-data').deleteOne({"_id": objectId(id)}, function(err, result) {
+            assert.equal(null, err);
+            console.log('Item deleted');
+            client.close();
+        });
+    });
 });
 
 module.exports = router;
